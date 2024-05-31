28ef4231d769b8369ac67599875aa351c6c5e3db
Keith <karirukeith@gmail.com>
2024-05-31 14:39:47 +0300

    fix: registered platform repository

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
+using PlatformService.Interfaces;
+using PlatformService.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMem"));
+
+builder.Services.AddScoped<IPlatformRepo, PlatformRepository>();
+
 
 var app = builder.Build();
 
