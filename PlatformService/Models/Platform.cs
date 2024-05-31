7cf33e0ebb263ca0a6d7b5ebafa1bf61d2e1c209
Keith <karirukeith@gmail.com>
2024-05-31 14:11:10 +0300

    Added the platform model

--- PlatformService/Models/Platform.cs
+++ PlatformService/Models/Platform.cs
@@ -1,6 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PlatformService.Models;
 
 public class Platform
 {
-    
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = String.Empty;
+
+    [Required]
+    public string Publisher { get; set; } = String.Empty;
+
+    [Required]
+    public string Cost { get; set; } = String.Empty;
 }
