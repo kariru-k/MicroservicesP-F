f0727d6d24246885010edc6f49215c659d9a7481
Keith <karirukeith@gmail.com>
2024-06-04 12:12:47 +0300

    Created commandservice dtos

---
+++ CommandsService/Dtos/CommandReadDto.cs
@@ -0,0 +1,12 @@
+namespace CommandsService.Dtos;
+
+public class CommandReadDto
+{
+    public int Id { get; set; }
+    
+    public string HowTo { get; set; } = String.Empty;
+
+    public string CommandLine { get; set; } = String.Empty;
+
+    public int PlatformId { get; set; }
+}
